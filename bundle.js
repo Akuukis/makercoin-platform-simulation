dbc9f2927a950cf8e7563d897e3e666fa3e8285c
Akuukis <akuukis@kalvis.lv>
2017-11-28 23:45:11 +0200

    Automatic update

--- bundle.js
+++ bundle.js
@@ -46992,173 +46992,6 @@ Popper.Defaults = Defaults;
 
 /***/ }),
 
-/***/ "../node_modules/prando/dist/Prando.es.js":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-var Prando = /** @class */ (function () {
-    function Prando(seed) {
-        if (seed === void 0) { seed = undefined; }
-        if (typeof (seed) === "string") {
-            // String seed
-            this._seed = this.hashCode(seed);
-        }
-        else if (typeof (seed) === "number") {
-            // Numeric seed
-            this._seed = seed;
-        }
-        else {
-            // Pseudo-random seed
-            this._seed = Date.now() + Math.random();
-        }
-        this.reset();
-    }
-    // ================================================================================================================
-    // PUBLIC INTERFACE -----------------------------------------------------------------------------------------------
-    /**
-     * Generates a pseudo-random number between a lower (inclusive) and a higher (exclusive) bounds.
-     *
-     * @param min - The minimum number that can be randomly generated.
-     * @param pseudoMax - The maximum number that can be randomly generated (exclusive).
-     * @return The generated pseudo-random number.
-     */
-    Prando.prototype.next = function (min, pseudoMax) {
-        if (min === void 0) { min = 0; }
-        if (pseudoMax === void 0) { pseudoMax = 1; }
-        this.recalculate();
-        return this.map(this._value, Prando.MIN, Prando.MAX, min, pseudoMax);
-    };
-    /**
-     * Generates a pseudo-random integer number in a range (inclusive).
-     *
-     * @param min - The minimum number that can be randomly generated.
-     * @param max - The maximum number that can be randomly generated.
-     * @return The generated pseudo-random number.
-     */
-    Prando.prototype.nextInt = function (min, max) {
-        if (min === void 0) { min = 10; }
-        if (max === void 0) { max = 100; }
-        this.recalculate();
-        return Math.floor(this.map(this._value, Prando.MIN, Prando.MAX, min, max + 1));
-    };
-    /**
-     * Generates a pseudo-random string sequence of a particular length from a specific character range.
-     *
-     * Note: keep in mind that creating a random string sequence does not guarantee uniqueness; there is always a
-     * 1 in (char_length^string_length) chance of collision. For real unique string ids, always check for
-     * pre-existing ids, or employ a robust GUID/UUID generator.
-     *
-     * @param length - Length of the strting to be generated.
-     * @param chars - Characters that are used when creating the random string. Defaults to all alphanumeric chars (A-Z, a-z, 0-9).
-     * @return The generated string sequence.
-     */
-    Prando.prototype.nextString = function (length, chars) {
-        if (length === void 0) { length = 16; }
-        if (chars === void 0) { chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; }
-        var str = "";
-        while (str.length < length) {
-            str += this.nextChar(chars);
-        }
-        return str;
-    };
-    /**
-     * Generates a pseudo-random string of 1 character specific character range.
-     *
-     * @param chars - Characters that are used when creating the random string. Defaults to all alphanumeric chars (A-Z, a-z, 0-9).
-     * @return The generated character.
-     */
-    Prando.prototype.nextChar = function (chars) {
-        if (chars === void 0) { chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; }
-        this.recalculate();
-        return chars.substr(this.nextInt(0, chars.length - 1), 1);
-    };
-    /**
-     * Picks a pseudo-random item from an array. The array is left unmodified.
-     *
-     * Note: keep in mind that while the returned item will be random enough, picking one item from the array at a time
-     * does not guarantee nor imply that a sequence of random non-repeating items will be picked. If you want to
-     * *pick items in a random order* from an array, instead of *pick one random item from an array*, it's best to
-     * apply a *shuffle* transformation to the array instead, then read it linearly.
-     *
-     * @param array - Array of any type containing one or more candidates for random picking.
-     * @return An item from the array.
-     */
-    Prando.prototype.nextArrayItem = function (array) {
-        this.recalculate();
-        return array[this.nextInt(0, array.length - 1)];
-    };
-    /**
-     * Generates a pseudo-random boolean.
-     *
-     * @return A value of true or false.
-     */
-    Prando.prototype.nextBoolean = function () {
-        this.recalculate();
-        return this._value > 0.5;
-    };
-    /**
-     * Skips ahead in the sequence of numbers that are being generated. This is equivalent to
-     * calling next() a specified number of times, but faster since it doesn't need to map the
-     * new random numbers to a range and return it.
-     *
-     * @param iterations - The number of items to skip ahead.
-     */
-    Prando.prototype.skip = function (iterations) {
-        if (iterations === void 0) { iterations = 1; }
-        while (iterations-- > 0) {
-            this.recalculate();
-        }
-    };
-    /**
-     * Reset the pseudo-random number sequence back to its starting seed. Further calls to next()
-     * will then produce the same sequence of numbers it had produced before. This is equivalent to
-     * creating a new Prando instance with the same seed as another Prando instance.
-     *
-     * Example:
-     * let rng = new Prando(12345678);
-     * console.log(rng.next()); // 0.6177754114889017
-     * console.log(rng.next()); // 0.5784605181725837
-     * rng.reset();
-     * console.log(rng.next()); // 0.6177754114889017 again
-     * console.log(rng.next()); // 0.5784605181725837 again
-     */
-    Prando.prototype.reset = function () {
-        this._value = this._seed;
-    };
-    // ================================================================================================================
-    // PRIVATE INTERFACE ----------------------------------------------------------------------------------------------
-    Prando.prototype.recalculate = function () {
-        // Xorshift*32
-        // Based on George Marsaglia's work: http://www.jstatsoft.org/v08/i14/paper
-        this._value ^= this._value << 13;
-        this._value ^= this._value >> 17;
-        this._value ^= this._value << 5;
-    };
-    Prando.prototype.map = function (val, minFrom, maxFrom, minTo, maxTo) {
-        return ((val - minFrom) / (maxFrom - minFrom)) * (maxTo - minTo) + minTo;
-    };
-    Prando.prototype.hashCode = function (str) {
-        var hash = 0;
-        if (str) {
-            var l = str.length;
-            for (var i = 0; i < l; i++) {
-                hash = ((hash << 5) - hash) + str.charCodeAt(i);
-                hash |= 0;
-            }
-        }
-        return hash;
-    };
-    Prando.MIN = -2147483648; // Int32 min
-    Prando.MAX = 2147483647; // Int32 max
-    return Prando;
-}());
-
-/* harmony default export */ __webpack_exports__["a"] = (Prando);
-//# sourceMappingURL=Prando.es.js.map
-
-
-/***/ }),
-
 /***/ "../node_modules/rafl/index.js":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -76882,17 +76715,17 @@ const styles = (theme) => ({
 let ProjectRow = class ProjectRow extends __WEBPACK_IMPORTED_MODULE_4__common___["a" /* ComponentRouted */] {
     render() {
         return (__WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableRow"], null,
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.name),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.birthTick),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.maturityTick),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.progress),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.maturity),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.discount),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.size),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.poisoned ? 'POISONED' : '-'),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.matured.toFixed(2)),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.value.toFixed(0)),
-            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], null, this.props.project.status)));
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense' }, this.props.project.name),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense', numeric: true }, this.props.project.birthTick),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense', numeric: true }, this.props.project.maturityTick),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense' }, this.props.project.progress),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense' }, this.props.project.maturity),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense', numeric: true }, this.props.project.discount.toFixed(2)),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense', numeric: true }, this.props.project.size),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense' }, this.props.project.poisoned ? 'POISONED' : '-'),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense', numeric: true }, this.props.project.matured.toFixed(2)),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense', numeric: true }, this.props.project.value.toFixed(0)),
+            __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_3_material_ui_Table__["TableCell"], { padding: 'dense' }, this.props.project.status)));
     }
 };
 ProjectRow = __decorate([
@@ -76951,17 +76784,17 @@ let Projects = class Projects extends __WEBPACK_IMPORTED_MODULE_6__common___["a"
             __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table___default.a, null,
                 __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableHead"], null,
                     __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableRow"], null,
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'name'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'birthTick'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'maturityTick'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'progress'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'maturity'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'discount'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'size'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'poisoned'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'matured'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'value'),
-                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], null, 'status'))),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'name'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'birthTick'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'maturityTick'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'progress'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'maturity'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'discount'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'size'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'poisoned'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'matured'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'value'),
+                        __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableCell"], { padding: 'dense' }, 'status'))),
                 __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_5_material_ui_Table__["TableBody"], null, [...this.case.projects].map((project) => (__WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_8__ProjectRow__["a" /* default */], { key: project.name, project: project })))))));
     }
 };
@@ -77162,9 +76995,8 @@ const routes = (__WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_I
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_mobx__ = __webpack_require__("../node_modules/mobx/lib/mobx.module.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__models_Project__ = __webpack_require__("./app/models/Project.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_prando__ = __webpack_require__("../node_modules/prando/dist/Prando.es.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_chance__ = __webpack_require__("../node_modules/chance/chance.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_chance___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_chance__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_chance__ = __webpack_require__("../node_modules/chance/chance.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_chance___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_chance__);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -77177,11 +77009,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
-
-const chance = __WEBPACK_IMPORTED_MODULE_3_chance__();
-function weighted(values) {
-    return chance.weighted(values, values.map((v) => v.weight));
-}
 class CaseStore {
     constructor(time, config) {
         this.time = time;
@@ -77191,8 +77018,11 @@ class CaseStore {
         time.tick$.subscribe((tick) => this.next());
     }
     get random() { return this._random; }
+    weighted(values) {
+        return chance.weighted(values, values.map((v) => v.weight));
+    }
     reset() {
-        this._random = new __WEBPACK_IMPORTED_MODULE_2_prando__["a" /* default */]();
+        this._random = new __WEBPACK_IMPORTED_MODULE_2_chance__();
         this.projects = [];
         this.platform = null;
         this.backer = null;
@@ -77204,20 +77034,21 @@ class CaseStore {
         }
         this.projects
             .filter((project) => project.maturityTick >= this.time.tick)
-            .filter(() => this.random.next() < this.config.defaultProb)
+            .filter(() => chance.bool({ likelihood: this.config.defaultProb }))
             .forEach(this.poisonProject, this);
         this.projects
             .filter((project) => project.maturityTick === this.time.tick)
             .forEach(this.matureProject, this);
     }
     spawnProject() {
-        const title = this.random.nextString(4);
-        const project = weighted(this.config.projectDist);
+        const name = this.random.name().split(' ')[0];
+        this.log.push({ name, event: 'spawn' });
+        const project = this.weighted(this.config.projectDist);
         const discount = project.value;
         const maturity = project.maturity;
         const progress = project.progress;
-        const size = weighted(this.config.idoSizeDist).value;
-        this.projects.push(new __WEBPACK_IMPORTED_MODULE_1__models_Project__["a" /* Project */](this.config, this.time, title, discount, maturity, progress, size));
+        const size = this.weighted(this.config.idoSizeDist).value;
+        this.projects.push(new __WEBPACK_IMPORTED_MODULE_1__models_Project__["a" /* Project */](this.config, this.time, name, discount, maturity, progress, size));
     }
     poisonProject(project) {
         this.log.push({ name: project.name, event: 'poison' });
